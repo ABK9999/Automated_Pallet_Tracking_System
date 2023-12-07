a473485241b62888900e41b85cd923000b24729d
ABK <bhargava07491@gmail.com>
2023-12-07 08:15:29 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -1,29 +1,11 @@
-import setuptools
+from setuptools import find_packages, setup
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+setup(
+    name = 'palletboxDetection',
+    version= '0.0.0',
+    author= 'Bhargav',
+    author_email= 'bhargava07491@gmail.com',
+    packages= find_packages(),
+    install_requires = []
 
-
-__version__ = "0.0.0"
-
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "Bhargav"
-SRC_REPO = "yolo"
-AUTHOR_EMAIL = "Bhargava07491@gmail.com"
-
-
-setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN app",
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
 )
--- template.py
+++ template.py
@@ -2,47 +2,54 @@ import os
 from pathlib import Path
 import logging
 
-#logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'yolo'
+
+project_name = "palletboxDetection"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constants/__init__.py",
-    "config/config.yaml",
-    "dvc.yaml",
-    "params.yaml",
+    "data/.gitkeep",
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/constant/__init__.py",
+    f"{project_name}/constant/training_pipeline/__init__.py",
+    f"{project_name}/constant/application.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifacts_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "app.py",
+    "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb",
-    "templates/index.html"
-
-
 ]
 
 
 for filepath in list_of_files:
     filepath = Path(filepath)
+
     filedir, filename = os.path.split(filepath)
 
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
 
-    if filedir !="":
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory; {filedir} for the file: {filename}")
-
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+    
+    if(not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
         with open(filepath, "w") as f:
             pass
-            logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filename}")
 
-
+    
     else:
-        logging.info(f"{filename} is already exists") 
+        logging.info(f"{filename} is already created")
